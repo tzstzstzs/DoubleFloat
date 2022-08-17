77e774702f2c54b31515a64349a5facea4bafe42
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2022-08-17 10:42:25 +0200

    Float

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double myPoundValue = 50d;
+        double myKgValue = myPoundValue * 0.45359237;
+
+        System.out.println(myKgValue);
+    }
+}
